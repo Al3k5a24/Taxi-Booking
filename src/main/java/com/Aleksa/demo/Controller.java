51318dbd13ecb69312afcec68835fcaae8a535be
Aleksa Djekic <84620255+Al3k5a24@users.noreply.github.com>
2025-05-28 23:11:36 +0200

    Notifications and Text update

--- src/main/java/com/Aleksa/demo/Controller.java
+++ src/main/java/com/Aleksa/demo/Controller.java
@@ -1,16 +1,13 @@
 package com.Aleksa.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
