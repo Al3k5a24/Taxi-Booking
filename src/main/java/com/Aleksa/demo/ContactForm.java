2f179e3d26e3c701d8e5a9b825e3cc1f571cf49b
Aleksa Djekic <leki.djekic@gmail.com>
2025-06-02 14:06:31 +0200

    Notifications and Email update

--- src/main/java/com/Aleksa/demo/ContactForm.java
+++ src/main/java/com/Aleksa/demo/ContactForm.java
@@ -1,26 +1,38 @@
 package com.Aleksa.demo;
 
-import jakarta.validation.constraints.Max;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
+@Entity
+@Table(name="contactform")
 public class ContactForm {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	@NotEmpty(message="Name can't be empty!")
 	@Size(min=2,max=30,message="Name must be between 2 and 30 characters!")
+	@Column(length=30)
 	private String name;
 	@NotEmpty(message="E-mail can't be empty!")
 	@Size(min=10,max=60,message="Email must be between 10 and 60 characters!")
+	@Column(length=60)
 	private String email;
 	@NotNull(message="Phone number can't be empty!")
-	@Min(value=1000000000,message="Phone must have at least 10 digits")
-	@Max(value=9999999999L,message="Phone must have at least 10 digits")
+	@Min(value=1000000,message="Phone must have at least 7 digits")
+	@Column(length=15)
 	private Long phone;
 	@NotEmpty(message="Message can't be empty!")
 	@Size(min=5,max=500,message="Message must be between 5 and 500 characters!")
+	@Column(length=500)
 	private String message;
 
 	public ContactForm(int id, String name, String email, Long phone, String message) {
