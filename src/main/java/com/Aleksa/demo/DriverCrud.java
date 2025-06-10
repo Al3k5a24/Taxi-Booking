2e8f0be7c9c42799574cf282d4439b99273ac44e
Aleksa Djekic <leki.djekic@gmail.com>
2025-06-10 18:16:19 +0200

    Update and Delete driver from table

---
+++ src/main/java/com/Aleksa/demo/DriverCrud.java
@@ -0,0 +1,23 @@
+package com.Aleksa.demo;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DriverCrud extends JpaRepository<Driver, Integer>{
+
+	@Override
+	public <S extends Driver> S save(S entity);
+	
+	@Override
+	public List<Driver> findAll();
+	
+	@Override
+	public void deleteById(Integer id);
+	
+	Optional<Driver> findById(Integer id);
+
+}
